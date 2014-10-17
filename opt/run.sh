bb9e15e6137efda2db67300f8ce92cb681c7c167
Tim Dettrick <tim@dettrick.net>
2014-10-17 14:25:11 +1000

    Starting containers if they already exist.

--- opt/run.sh
+++ opt/run.sh
@@ -10,15 +10,18 @@ then
     exit 1
 fi
 
-# Create gatehouse and machineshop servers
-docker run -d --name dit4c_gatehouse \
+# Start or create gatehouse and machineshop servers
+docker start dit4c_gatehouse || \
+  docker run -d --name dit4c_gatehouse \
     -p 80:80 \
     -v /var/log/dit4c_gatehouse/supervisor:/var/log/supervisor \
     -v /var/log/dit4c_gatehouse/nginx:/var/log/nginx \
     -e PORTAL_URL=$PORTAL_URL \
     -v $DOCKER_SOCKET:$DOCKER_SOCKET \
     dit4c/dit4c-platform-gatehouse
-docker run -d --name dit4c_machineshop \
+    
+docker start dit4c_machineshop || \
+  docker run -d --name dit4c_machineshop \
     -p 8080:8080 \
     -v /var/log/dit4c_machineshop/supervisor:/var/log/supervisor \
     -e PORTAL_URL=$PORTAL_URL \
