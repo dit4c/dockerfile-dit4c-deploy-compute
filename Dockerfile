945887bf8b19fe8d0970daecf1f08986b2999eaf
Tim Dettrick <t.dettrick@uq.edu.au>
2015-08-14 16:50:19 +1000

    Slimming image, and adding convenience overrides

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 # DOCKER-VERSION 1.1.0
-FROM centos:centos7
+FROM alpine:3.2
 MAINTAINER t.dettrick@uq.edu.au
 
 # Set defaults which should be overridden on run
 ENV PORTAL_URL https://dit4c.metadata.net
 
-RUN yum install -y docker
+RUN apk add --update docker
 
 ADD /opt /opt
 
-CMD ["bash", "/opt/run.sh"]
+CMD ["sh", "/opt/run.sh"]
--- opt/run.sh
+++ opt/run.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Setting up for portal at $PORTAL_URL"
 
+PROXY_PORT=${PROXY_PORT:-80}
+DIT4C_VERSION=${DIT4C_VERSION:-"latest"}
 DOCKER_SOCKET="/var/run/docker.sock"
 
 if [ ! -S $DOCKER_SOCKET ]
@@ -13,18 +15,22 @@ fi
 # Start or create gatehouse and machineshop servers
 docker start dit4c_gatehouse || \
   docker run -d --name dit4c_gatehouse \
-    -p 80:80 \
-    -v /var/log/dit4c_gatehouse/supervisor:/var/log/supervisor \
-    -v /var/log/dit4c_gatehouse/nginx:/var/log/nginx \
     -e PORTAL_URL=$PORTAL_URL \
     -v $DOCKER_SOCKET:$DOCKER_SOCKET \
-    dit4c/dit4c-platform-gatehouse
-    
+    dit4c/dit4c-platform-gatehouse:$DIT4C_VERSION
+
 docker start dit4c_machineshop || \
   docker run -d --name dit4c_machineshop \
-    -p 8080:8080 \
     -v /opt/dit4c-machineshop:/etc/dit4c-machineshop \
     -v /var/log/dit4c_machineshop/supervisor:/var/log/supervisor \
     -e PORTAL_URL=$PORTAL_URL \
     -v $DOCKER_SOCKET:$DOCKER_SOCKET \
-    dit4c/dit4c-platform-machineshop
+    dit4c/dit4c-platform-machineshop:$DIT4C_VERSION
+
+docker start dit4c_cnproxy || \
+  docker run -d --name dit4c_cnproxy \
+    -p ${PROXY_PORT}:8080 \
+    --link dit4c_gatehouse:gatehouse \
+    --link dit4c_machineshop:machineshop \
+    -e PORTAL_URL=$PORTAL_URL \
+    dit4c/dit4c-platform-cnproxy
